bc711010d8a61f22d3396a2fe841a56d85ff33d7
yadavjyoti1018 <134961628+yadavjyoti1018@users.noreply.github.com>
2025-08-19 09:51:54 +0530

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Jyoti Yadav on 19/08/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
